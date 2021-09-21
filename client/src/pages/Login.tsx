854666104ed1691175a6727378bb7ac2dabc6026
orcanicman <bjornpostemaster@gmail.com>
2021-09-21 12:38:20 +0200

    finished login/register

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,26 +1,31 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 interface LoginProps {}
 
 export const Login: React.FC<LoginProps> = () => {
   return (
     <div className="flex">
-      <div className="w-1/4"></div>
-      <div className="w-2/4 flex flex-col items-center">
-        <h1 className="text-4xl font-bold m-4">Login</h1>
-        <form className="flex flex-col items-center mt-16">
-          <label className="font-bold">Username</label>
-          <input className="border-b mb-8" placeholder="username" />
-          <label className="font-bold">Password</label>
+      <div className="flex-shrink sm:w-64"></div>
+      <div className="flex-grow flex flex-col p-4 sm:p-0 sm:items-center">
+        <form className="flex flex-col mt-8 sm:w-80">
+          <h1 className="text-4xl font-bold mb-8 self-start">Login</h1>
+          <label className="font-bold self-start pb-1">Username</label>
+          <input className="border p-1 mb-8" placeholder="username" />
+          <label className="font-bold self-start pb-1">Password</label>
           <input
-            className="border-b mb-8"
+            className="border p-1 mb-8"
             placeholder="password"
             type="password"
           />
-          <button className="border-2 border-black rounded p-1">Submit</button>
+          <button className="border-2 border-black p-1">Submit</button>
+          <div className="flex flex-wrap justify-between mt-4">
+            <span className="underline pr-4">Forgot password?</span>
+            <Link to="/register"><span className="underline">Register account</span></Link>
+          </div>
         </form>
       </div>
-      <div className="w-1/4"></div>
+      <div className="flex-shrink sm:w-64"></div>
     </div>
   );
 };
