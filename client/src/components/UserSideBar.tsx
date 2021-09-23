43632587c0c0690d9343ff025167b83397c4f0f1
Coyu97 <106992@student.drenthecollege.nl>
2021-09-23 14:21:51 +0200

    Sidebar content and styling

--- client/src/components/UserSideBar.tsx
+++ client/src/components/UserSideBar.tsx
@@ -1,7 +1,18 @@
 import React from "react";
+import defaultImg from "../Images/default.jpg";
 
 interface UserSideBarProps {}
 
 export const UserSideBar: React.FC<UserSideBarProps> = () => {
-  return <div className="flex fixed">testing</div>;
+  return (
+    <div className="flex flex-col w-full items-center">
+      <img src={defaultImg} alt="" className="w-3/4 h-3/4 rounded-full p-4" />
+
+      <div className="text-lg hover:bg-gray w-3/4 p-2 rounded-md">Username</div>
+      <div className="text-lg hover:bg-gray w-3/4 p-2">UserTag</div>
+      <div className="text-lg hover:bg-gray w-3/4 p-2">Biography</div>
+      <div className="text-lg hover:bg-gray w-3/4 p-2">foto</div>
+      <div className="text-lg hover:bg-gray w-3/4 p-2">Settings</div>
+    </div>
+  );
 };
