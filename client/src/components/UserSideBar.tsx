85aa66de6be43167485c28dca3b123ec13579ead
Coyu97 <106992@student.drenthecollege.nl>
2021-10-07 13:05:03 +0200

    Username and Usertag in sidebar

--- client/src/components/UserSideBar.tsx
+++ client/src/components/UserSideBar.tsx
@@ -7,7 +7,9 @@ export const UserSideBar: React.FC<UserSideBarProps> = () => {
   return (
     <div className="flex flex-col w-full items-center">
       <img src={defaultImg} alt="" className="  w-3/4 rounded-full p-4" />
-
+      <div className="text-lg ">Coyu</div>
+      <div className="text-sm font-light">@Skankhunter42</div>
+      <div className=" w-3/4 p-2">Dit is een bio</div>
       <div className="text-lg hover:bg-gray w-3/4 p-2 rounded-md cursor-pointer">
         Username
       </div>
