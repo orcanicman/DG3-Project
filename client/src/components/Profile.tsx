3aa240cf632834e6184f08e6a87490ff00a6e7ef
Coyu97 <106992@student.drenthecollege.nl>
2021-11-24 09:29:17 +0100

    profilepage testing

--- client/src/components/Profile.tsx
+++ client/src/components/Profile.tsx
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useContext } from "react";
+import { UserContext } from "../context/UserContext";
 import bannerImg from "../Images/banner.jpg";
 import profilePicture from "../Images/default.jpg";
 
 interface ProfileProps {}
 
 export const Profile: React.FC<ProfileProps> = () => {
+  const { state, dispatch } = useContext(UserContext);
+  const user = state.user;
+
   return (
     <div className="flex flex-col w-full bg-gray rounded">
       <div className="relative w-full">
@@ -15,7 +19,17 @@ export const Profile: React.FC<ProfileProps> = () => {
           className="w-1/4 rounded-full  absolute top-1/2 border-8 m-2 border-lightGray"
         />
       </div>
-      <div className=""></div>
+      <div className="border flex flex-col">
+        <div className="border flex py-10">
+          <div className="w-72"></div>
+          <div>Bio</div>
+        </div>
+        <div className="border flex justify-between">
+          <div>1</div>
+          <div>2</div>
+          <div>3</div>
+        </div>
+      </div>
     </div>
   );
 };
